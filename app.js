20a3edb64c37f35f52ef956c37d36df5ee83aa2c
Richard <r.richard0000@gmail.com>
2020-04-25 17:06:48 -0300

    Basic app. Docker and Docker Compose running. Postgres container configured. Mailcatcher container configured.

---
+++ app.js
@@ -0,0 +1,8 @@
+{
+  "name": "Virtual Prayers",
+  "description": "An app to let the preayers spread in the correct direction, the share the results",
+  "image": "heroku/ruby",
+  "addons": [
+    "heroku-postgresql"
+  ]
+}
